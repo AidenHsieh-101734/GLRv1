5ec11030b66e33f510b30293e24272eb1ecb26eb
otisvdstelt <101758@glr.nl>
2025-01-29 09:23:57 +0100

    Add files via upload

---
+++ Glr/JavaScript/Info.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+
+    if(window.document = "Jongeren.html"){
+        let Login = document.getElementById("Jongeren");
+        Login.style.border = "green";
+        Login.style.paddingTop = '70px';
+        Login.style.paddingBottom = '10px';
+        Login.style.backgroundColor = '#9acc52';
+        Login.style.color = "white"
+    }
+}
+function Home(){
+    window.location.href = "Home.html"
+}
+function Jongeren(){
+    window.location.href = "Jongeren.html"
+}
+function Stem(){
+    window.location.href = "Stem.html"
+}
+function Uitslag(){
+    window.location.href = "Uitslag.html"
+}
+function Login(){
+    window.location.href = "Login.html"
+}
+function Admin(){
+    window.location.href = "Admin.html"
+}
